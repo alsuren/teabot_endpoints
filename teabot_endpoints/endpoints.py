d7f5368281b9366a3db3e039793b6759b08b1ddd
David Laban <alsuren@gmail.com>
2017-05-21 10:40:40 +0100

    try serving some statics from flask (I know: I should us nginx for this)

--- teabot_endpoints/endpoints.py
+++ teabot_endpoints/endpoints.py
@@ -8,7 +8,7 @@ import json
 from datetime import datetime
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='teabot_stats')
 slack_communicator_wrapper = SlackCommunicator()
 
 
